1a49ea961bc7fd9741966eb9330dadc10dcdab20
mpubg <135715389+mpubg@users.noreply.github.com>
2026-02-04 01:34:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/lookup", async (req, res) => {
         "Offers.Listings.Price",
         "Images.Primary.Large",
         "Images.Variants.Large",
-        "VariationSummary"
+        "VariationSummary",
       ],
     };
 
@@ -66,7 +66,7 @@ app.get("/lookup", async (req, res) => {
         "Offers.Listings.Price",
         "Images.Primary.Large",
         "Images.Variants.Large",
-        "VariationSummary"
+        "VariationSummary",
       ],
     };
 
