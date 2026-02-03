4706e4c2e1a1a32307fe1d62929eab5832e9fd67
mpubg <135715389+mpubg@users.noreply.github.com>
2026-02-04 02:08:28 +0530

    Update items.js

--- api/items.js
+++ api/items.js
@@ -27,7 +27,8 @@ export default async function handler(req, res) {
         "Offers.Listings.Price",
         "ItemInfo.ProductInfo",
         "ItemInfo.ContentInfo",
-        "ItemInfo.ManufactureInfo"
+        "ItemInfo.ManufactureInfo",
+        "ItemInfo.TradeInInfo"
       ],
     };
 
